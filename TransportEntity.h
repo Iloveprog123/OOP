5e8cc0c5f6906bc43b825e79a765a443925d6bf2
brytieng <146850170+brytieng@users.noreply.github.com>
2025-06-08 20:06:40 +0800

    Create TransportEntity.h

---
+++ TransportEntity.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+class TransportEntity
+{
+private:
+	std::string id;
+	std::string location;
+	std::string time;
+
+public:
+	std::string getID();
+	std::string getLocation();
+	std::string getTime();
+	void setID(std::string);
+	void setLocation(std::string);
+	void setTime(std::string);
+};
+
+
